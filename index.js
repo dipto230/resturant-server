369e66b559bd37f828e1ff994d03d19f6d17b88a
dipto230 <diptoroy230305@gmail.com>
2025-01-12 04:36:06 +0600

    server side update

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ require('dotenv').config()
 const port =process.env.PORT || 5000;
 
 
+
+
 //middleware
 app.use(cors());
 app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.x2qb8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,11 +31,37 @@ async function run() {
     await client.connect();
 
   const menuCollection = client.db("BistroDB").collection("menu");
+  const reviewCollection = client.db("BistroDB").collection("reviews");
+  const cartCollection = client.db("BistroDB").collection("carts");
 
   app.get('/menu', async(req, res)=>{
     const result = await menuCollection.find().toArray();
     res.send(result);
   })
+  app.get('/reviews', async(req, res)=>{
+    const result = await reviewCollection.find().toArray();
+    res.send(result);
+  })
+ //cart collection
+ app.get('/carts',async(req,res)=>{
+  const email =req.query.email;
+  const query ={email:email};
+
+  const result = await cartCollection.find(query).toArray()
+  res.send(result);
+ })
+ app.post('/carts', async(req, res)=>{
+  const cartItem =req.body;
+  const result = await cartCollection.insertOne(cartItem);
+  res.send(result);
+ });
+ app.delete('/carts/:id', async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) }
+  const result = await cartCollection.deleteOne(query);
+  res.send(result);
+})
+
 
 
 
@@ -53,3 +81,4 @@ app.get('/',(req,res)=>{
 app.listen(port ,()=>{
     console.log(`Bistro boss is siting on port ${port}`);
 })
+
