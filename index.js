ccdc26909a8c081920d13aaeabcb93dacac95e44
dipto230 <diptoroy230305@gmail.com>
2025-02-01 20:57:57 +0600

    code updated succesfully

--- index.js
+++ index.js
@@ -237,6 +237,33 @@ async function run() {
             res.send({paymentResult, deleteResult});
 
         })
+
+    app.get('/admin-stats', verifyToken, verifyAdmin, async(req, res)=>{
+        const users = await userCollection.estimatedDocumentCount();
+        const menuItems = await menuCollection.estimatedDocumentCount();
+        const orders = await paymentCollection.estimatedDocumentCount();
+
+        // const payments = await paymentCollection.find().toArray();
+        // const revenue = payments.reduce((total , payment)=> total + payment.price, 0);
+        const result = await paymentCollection.aggregate([
+            {
+                $group:{
+                    _id:null,
+                    totalRevenue:{
+                        $sum:'$price'
+                    }
+                }
+            }
+        ]).toArray();
+        const revenue = result.length >0 ? result[0].totalRevenue : 0;
+
+        res.send({
+            users,
+            menuItems,
+            orders,
+            revenue
+        })
+    })
         
 
         await client.db("admin").command({ ping: 1 });
